2cd24790e2927ce721048fe465d1a2ffa81e9682
Pontus Nilsson <me@nilssonpontus.com>
2022-09-11 01:02:55 +0200

    Refactor listing module

--- src/modules/listingImprovements.lua
+++ src/modules/listingImprovements.lua
@@ -16,8 +16,10 @@ local wipe = wipe
 local hooksecurefunc = hooksecurefunc
 
 function ListingImprovements:OnInitialize()
-    self._lockedActivityIds = {}
+    self._lockedInstanceIds = {}
 	self._savedInstances = {}
+	self._dataRangeCallbackRegistered = false
+	self._lastCategoryId = nil
 end
 
 function ListingImprovements:OnEnable()
@@ -27,9 +29,30 @@ function ListingImprovements:OnEnable()
 	self:OnConfigChanged()
 end
 
-function ListingImprovements:DisableRoleSelection()
+function ListingImprovements:OnPlayerEnteringWorld()
+	self:UpdateAllowedRoles()
+	self:RefreshSavedInstances()
+
+	hooksecurefunc("LFGListingActivityView_UpdateActivities", function(frame, categoryId)
+		self:OnUpdateListningsActivities(frame, categoryId)
+	end)
+	hooksecurefunc(LFGListingFrame, "SetAllActivitiesForActivityGroup", function(...)
+		self:OnSetAllActivitiesForActivityGroup(...)
+	end)
+
+	hooksecurefunc('LFGListingActivityView_InitActivityGroupButton', function(button, _, isCollapsed)
+		button.CheckButton:SetDisabledTexture("Interface\\Buttons\\LockButton-Locked-Up")
+		button.CheckButton:GetDisabledTexture():SetDesaturated(true)
+	end)
+end
+
+function ListingImprovements:OnConfigChanged(event, category, key, value, ...)
+end
+
+function ListingImprovements:UpdateAllowedRoles()
 	local _, class, _ = UnitClass("player")
 
+	-- Find the "role" frames in the LFG UI
 	local children = { LFGListingFrameSoloRoleButtons:GetChildren() }
 	for _, frame in pairs(children) do
 		if not Const.Roles[frame.roleID][class] then
@@ -46,48 +69,62 @@ function ListingImprovements:DisableRoleSelection()
 	end
 end
 
-function ListingImprovements:OnPlayerEnteringWorld()
-    self:DisableRoleSelection()
+function ListingImprovements:RefreshSavedInstances()
+	wipe(self._savedInstances)
 
-    wipe(self._savedInstances)
-
-    local numSavedInstances = GetNumSavedInstances()
+	local numSavedInstances = GetNumSavedInstances()
 	for i = 1, numSavedInstances do
 		local name = GetSavedInstanceInfo(i)
 		self._savedInstances[name] = true
 	end
-
-    hooksecurefunc("LFGListingActivityView_UpdateActivities", function(frame, categoryId) self:OnUpdateListningsActivities(frame, categoryId) end)
-	hooksecurefunc(LFGListingFrame, "SetAllActivitiesForActivityGroup", function(...) self:OnSetAllActivitiesForActivityGroup(...) end)
 end
 
-function ListingImprovements:OnConfigChanged(event, category, key, value, ...)
-end
+function ListingImprovements:LockSavedInstances(categoryId)
+	local getAvailableActivities = C_LFGList.GetAvailableActivities
+	local getActivityInfoTable = C_LFGList.GetActivityInfoTable
+	local lockedInstanceIds = self._lockedInstanceIds
 
-function ListingImprovements:OnUpdateListningsActivities(frame, categoryId)
-	wipe(self._lockedActivityIds)
+	wipe(lockedInstanceIds)
 
 	local activityGroups = C_LFGList.GetAvailableActivityGroups(categoryId)
 	for i = 1, #activityGroups do
-		local activityGroupID = activityGroups[i]
-		local activities = C_LFGList.GetAvailableActivities(categoryId, activityGroupID)
-		if (#activities > 0) then
-			self._lockedActivityIds[activityGroupID] = {}
-
+		local activityGroupId = activityGroups[i]
+		local activities = getAvailableActivities(categoryId, activityGroupId)
+		if #activities > 0 then
+			lockedInstanceIds[activityGroupId] = {}
 			for j = 1, #activities do
 				local activityId = activities[j]
-				local activityInfo = C_LFGList.GetActivityInfoTable(activityId)
+				local activityInfo = getActivityInfoTable(activityId)
+
 				local name = activityInfo.shortName ~= "" and activityInfo.shortName or activityInfo.fullName
 				if self._savedInstances[name] then
-					self._lockedActivityIds[activityGroupID][activityId] = name
+					self._lockedInstanceIds[activityGroupId][activityId] = name
 				end
 			end
 		end
+	end
+end
+
+function ListingImprovements:OnUpdateListningsActivities(frame, categoryId)
+	self:LockSavedInstances(categoryId)
+
+	if self._lastCategoryId ~= categoryId then
+		self:OnDataRangeChanged(frame)
+		self._lastCategoryId = categoryId
 	end
 
 	frame.ScrollBox:RegisterCallback(frame.ScrollBox.Event.OnDataRangeChanged, function(...)
 		self:OnDataRangeChanged(frame)
 	end, self)
+end
+
+function ListingImprovements:OnSetAllActivitiesForActivityGroup(frame, activityGroupID, selected, userInput)
+	local activityIds = self._lockedInstanceIds[activityGroupID]
+	if activityIds then
+		for id, _ in pairs(activityIds) do
+			frame:SetActivity(id, false)
+		end
+	end
 end
 
 local frameLookup = {}
@@ -97,34 +134,30 @@ function ListingImprovements:OnDataRangeChanged(scrollFrame)
 	local frames = scrollFrame.ScrollBox:GetFrames()
 	for i = 1, #frames do
 		local frame = frames[i]
-		frame.CheckButton:Enable()
-		frame.NameButton:Enable()
-		frame.NameButton.Name:SetFontObject("GameFontNormal")
+		self:SetScrollBoxEntryEnabled(frame, true)
 		frameLookup[frame.NameButton.Name:GetText()] = i
 	end
 
-	for _, groupData in pairs(self._lockedActivityIds) do
+	for _, groupData in pairs(self._lockedInstanceIds) do
 		for activityId, name in pairs(groupData) do
 			local frame = frames[frameLookup[name]]
 			if frame then
-				frame.NameButton.Name:SetFontObject("GameFontDisable")
-				frame.CheckButton:SetDisabledTexture("Interface\\Buttons\\LockButton-Locked-Up")
-				frame.CheckButton:GetDisabledTexture():SetDesaturated(true)
-
-				frame.CheckButton:Disable()
-				frame.NameButton:Disable()
-
-				LFGListingFrame:SetActivity(activityId, false, false)
+				self:SetScrollBoxEntryEnabled(frame, false, activityId)
 			end
 		end
 	end
 end
 
-function ListingImprovements:OnSetAllActivitiesForActivityGroup(frame, activityGroupID, selected, userInput)
-	local activityIds = self._lockedActivityIds[activityGroupID]
-	if activityIds then
-		for id, _ in pairs(activityIds) do
-			frame:SetActivity(id, false)
-		end
+function ListingImprovements:SetScrollBoxEntryEnabled(frame, enabled, activityId)
+	if enabled then
+		frame.CheckButton:Enable()
+		frame.NameButton:Enable()
+		frame.NameButton.Name:SetFontObject("GameFontNormal")
+	else
+		frame.CheckButton:Disable()
+		frame.NameButton:Disable()
+		frame.NameButton.Name:SetFontObject("GameFontDisable")
+
+		LFGListingFrame:SetActivity(activityId, false, false)
 	end
 end
